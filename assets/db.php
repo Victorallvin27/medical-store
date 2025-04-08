4325d303e3a7611030211f675e7a144a6d1b9d5f
Victorallvin27 <allvinvictor.com@gmail.com>
2025-04-08 12:44:53 +0530

    db.php

--- assets/db.php
+++ assets/db.php
@@ -1,6 +1,6 @@
 <?php 
    $schema='medicalStore';
-   $con = new mysqli('13.202.48.4','admin','rdsConnect',$schema,3309);
+   $con = new mysqli('database-1.cxk846iyaaig.ap-south-1.rds.amazonaws.com','admin','rdsConnect',$schema,3309);
 //for user informationa making available for all pages
 
     $array = $con->query("select * from users where id ='$_SESSION[userId]'");
