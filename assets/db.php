ce48f202980abcf3a7cbf524753e0623cdaab596
Victorallvin27 <allvinvictor.com@gmail.com>
2025-04-08 12:25:52 +0530

    db.php

--- assets/db.php
+++ assets/db.php
@@ -1,10 +1,10 @@
 <?php 
    $schema='medicalStore';
-   $con = new mysqli('localhost','root','',$schema);
+   $con = new mysqli('13.202.48.4','admin','rdsConnect',$schema,3309);
 //for user informationa making available for all pages
 
     $array = $con->query("select * from users where id ='$_SESSION[userId]'");
     $user = $array->fetch_assoc();
 
-	$base_path = "http://localhost/dev.test/store";
+	$base_path = "http://13.202.48.4/dev.test/store";
 ?>
